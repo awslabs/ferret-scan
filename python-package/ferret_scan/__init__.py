e3669e87cd8c6c37313990ae030c43ddd2a4ae97
Andrea Di Fabio <adifabio@amazon.com>
2025-10-29 13:26:37 -0400

    chore: update Python package version to 1.2.6

--- python-package/ferret_scan/__init__.py
+++ python-package/ferret_scan/__init__.py
@@ -8,5 +8,5 @@ A Python wrapper for the Ferret Scan Go binary that provides
 easy installation and pre-commit hook integration.
 """
 
-__version__ = "1.2.5"
+__version__ = "1.2.6"
 __author__ = "AWS"
